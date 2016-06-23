30d1001434ebdf08695f197107f52e313ffb7250
Alex Coleman <alexanderwinstoncoleman@gmail.com>
2016-06-23 16:23:08 -0400

    vroom vroom

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Here are some handy dandy utility helper functions */
+
+/* dump(); - makes for easy debugging. <?php dump($post); ?> */
+function dump($obj) {
+	echo "<pre>";
+	print_r($obj);
+	echo "</pre>";
+}
