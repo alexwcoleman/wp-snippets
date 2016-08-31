f93d4423996276201b88f3bbad932648b2572e73
Alex Coleman <alexanderwinstoncoleman@gmail.com>
2016-08-31 14:04:15 -0400

    paging navigation added

--- functions.php
+++ functions.php
@@ -116,3 +116,60 @@ function my_login_logo_url_title() {
 return 'Your Site Name and Info';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+////////////////////////////
+// PAGING NAVIGATION
+// SEE: https://mor10.com/add-proper-pagination-default-wordpress-themes/
+////////////////////////////
+
+if ( ! function_exists( 'yourtheme_paging_nav' ) ) :
+/**
+ * Display navigation to next/previous set of posts when applicable.
+ * Based on paging nav function from Twenty Fourteen
+ */
+
+function yourtheme_paging_nav() {
+    // Don't print empty markup if there's only one page.
+    if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+        return;
+    }
+
+    $paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+    $pagenum_link = html_entity_decode( get_pagenum_link() );
+    $query_args   = array();
+    $url_parts    = explode( '?', $pagenum_link );
+
+    if ( isset( $url_parts[1] ) ) {
+        wp_parse_str( $url_parts[1], $query_args );
+    }
+
+    $pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+    $pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+    $format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+    $format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+
+    // Set up paginated links.
+    $links = paginate_links( array(
+        'base'     => $pagenum_link,
+        'format'   => $format,
+        'total'    => $GLOBALS['wp_query']->max_num_pages,
+        'current'  => $paged,
+        'mid_size' => 3,
+        'add_args' => array_map( 'urlencode', $query_args ),
+        'prev_text' => __( '&larr; Previous', 'yourtheme' ),
+        'next_text' => __( 'Next &rarr;', 'yourtheme' ),
+        'type'      => 'list',
+    ) );
+
+    if ( $links ) :
+
+    ?>
+    <nav class="navigation paging-navigation" role="navigation">
+        <h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'yourtheme' ); ?></h1>
+            <?php echo $links; ?>
+    </nav><!-- .navigation -->
+    <?php
+    endif;
+}
+endif;
